bda3085233c87d4e5421ae882cdd7361d652c673
odidev <odidev@puresoftware.com>
2021-02-19 10:10:36 +0000

    Test Docker image for ARM64
    Signed-off-by: odidev <odidev@puresoftware.com>

--- deploycontainer.sh
+++ deploycontainer.sh
@@ -6,8 +6,8 @@ GIT_COMMIT=$2
 DOCKER_USERNAME=$3
 DOCKER_PASSWORD=$4
 
-if [ $BRANCH = "master" ]; then
-    BRANCH=microsoft
+if [ $BRANCH = "local_test" ]; then
+    BRANCH=odidev
 fi
 
 if [ -z $GIT_COMMIT ]; then
@@ -27,20 +27,8 @@ dotnet publish -c $config sfintegration -f netcoreapp2.1 -r ubuntu.16.04-x64 --s
 
 # Pull the previous image to speed up image generation
 docker pull $BRANCH/service-fabric-reverse-proxy:latest
+docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
 
 # Build the Docker images
 echo docker build -t $BRANCH/service-fabric-reverse-proxy:$TAG --label GIT_COMMIT=$GIT_COMMIT ./sfintegration/bin/$config/netcoreapp2.1/ubuntu.16.04-x64/publish/.
-docker build -t $BRANCH/service-fabric-reverse-proxy:$TAG --label GIT_COMMIT=$GIT_COMMIT ./sfintegration/bin/$config/netcoreapp2.1/ubuntu.16.04-x64/publish/.
-echo docker tag $BRANCH/service-fabric-reverse-proxy:$TAG $BRANCH/service-fabric-reverse-proxy:xenial-$TAG
-docker tag $BRANCH/service-fabric-reverse-proxy:$TAG $BRANCH/service-fabric-reverse-proxy:xenial-$TAG
-echo docker tag $BRANCH/service-fabric-reverse-proxy:$TAG $BRANCH/service-fabric-reverse-proxy:latest
-docker tag $BRANCH/service-fabric-reverse-proxy:$TAG $BRANCH/service-fabric-reverse-proxy:latest
-
-# Login to Docker Hub and upload images
-echo $DOCKER_PASSWORD | docker login -u="$DOCKER_USERNAME" --password-stdin
-echo docker push $BRANCH/service-fabric-reverse-proxy:$TAG
-docker push $BRANCH/service-fabric-reverse-proxy:$TAG
-echo docker push $BRANCH/service-fabric-reverse-proxy:xenial-$TAG
-docker push $BRANCH/service-fabric-reverse-proxy:xenial-$TAG
-echo docker push $BRANCH/service-fabric-reverse-proxy:latest
-docker push $BRANCH/service-fabric-reverse-proxy:latest
+docker buildx build --push --platform linux/amd64,linux/arm64 -t $BRANCH/service-fabric-reverse-proxy:$TAG -t $BRANCH/service-fabric-reverse-proxy:xenial-$TAG -t $BRANCH/service-fabric-reverse-proxy:latest --label GIT_COMMIT=$GIT_COMMIT ./sfintegration/bin/$config/netcoreapp2.1/ubuntu.16.04-x64/publish/.
